ce09d659afa871d3403c326c47941e946ebf8872
Lzyct <hey.mudassir@gmail.com>
2022-07-06 01:24:25 +0800

    [FEAT] Add String

---
+++ 1.Go-Lang Basic/string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Lzyct")
+	fmt.Println("Lenght from Lzyct ", len("Lzyct")) //5
+	fmt.Println("Index 0 from Lzyct", "Lzyct"[0])   // 76 : byte number for L
+	fmt.Println("Index 1 from Lzyct", "Lzyct"[1])   // 122 : byte number for z
+
+}
