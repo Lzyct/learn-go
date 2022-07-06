165ca9743a00174ada1b6ac6806861617394dd99
Lzyct <hey.mudassir@gmail.com>
2022-07-07 07:15:13 +0800

    [FEAT] Add new sample on array

--- 1.Go-Lang Basic/array.go
+++ 1.Go-Lang Basic/array.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func main() {
+	// if you define len from array
+	// you can't append new data in numbers
 	var numbers = [5]int{
 		1, 2, 3, 4,
 	}
 
-	fmt.Println(numbers)      // [1 2 3 4 0]
+	// if you not define len from array
+	// you can append new data in numbers
+	var unknownNumbers = [...]int{
+		5, 4, 3, 2, 1,
+	}
+
+	fmt.Println(numbers) // [1 2 3 4 0]
+	numbers[4] = 9
 	fmt.Println(numbers[3])   // 4
-	fmt.Println(numbers[4])   // 0
+	fmt.Println(numbers[4])   // 9
 	fmt.Println(len(numbers)) // 5 : lenght from numbers
+
+	fmt.Println(unknownNumbers)
+
 }
