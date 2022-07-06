71cfd2c454a9085ca1ca806f54246b90de42c0eb
Lzyct <hey.mudassir@gmail.com>
2022-07-07 05:51:29 +0800

    [FEAT] Add type-declarations

---
+++ 1.Go-Lang Basic/type-declarations.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	type char string
+    type handsome bool
+
+	var name char = "Lzyct"
+	fmt.Println(name)
+
+	var isHandsome handsome = true;
+	fmt.Println(isHandsome)
+
+}
