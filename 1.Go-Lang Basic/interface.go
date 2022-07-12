2892a31b21023f01b9ea17c1eca7fce8fb5b6475
Lzyct <hey.mudassir@gmail.com>
2022-07-12 21:37:45 +0800

    [FEAT] Add interface

---
+++ 1.Go-Lang Basic/interface.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type GithubInterface interface {
+	GetUserName() string
+}
+
+func showName(githubInterface GithubInterface) {
+	fmt.Println("Hello", githubInterface.GetUserName())
+}
+
+type GithubUsername struct {
+	Name string
+}
+type Animal struct {
+	Name string
+}
+
+func (githubUsername GithubUsername) GetUserName() string {
+	return githubUsername.Name
+}
+
+func (animal Animal) GetUserName() string {
+	return animal.Name
+}
+
+func main() {
+	githubUserName := GithubUsername{
+		Name: "Lzyct",
+	}
+	showName(githubUserName)
+
+	animal := Animal{
+		Name: "Cat",
+	}
+	showName(animal)
+}
