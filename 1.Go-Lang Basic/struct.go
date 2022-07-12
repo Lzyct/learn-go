e7765657e83f39b9e85594b13fc57082d6a6126a
Lzyct <hey.mudassir@gmail.com>
2022-07-12 21:25:13 +0800

    [FEAT] Update struct, add example struct method

--- 1.Go-Lang Basic/struct.go
+++ 1.Go-Lang Basic/struct.go
@@ -7,12 +7,26 @@ type GithubUser struct {
 	Stars          int
 }
 
+/**
+Struct function, is like method inside struct
+but actually it's same like this
+
+func structFunction(githubUser GithubUser,name string)
+
+structFunction(style2,"ukieTux")
+
+You can using struct function to create function for specific struct.
+*/
+func (githubUser GithubUser) structFunction(name string) {
+	fmt.Println("Hello", name, "Please follow", githubUser.UserName, "on Github")
+}
+
 func main() {
 	var style1 GithubUser
 	style1.UserName = "Lzyct"
 	style1.Name = "Mudassir"
 	style1.Stars = 69
-	fmt.Println("Style 1 :", style1)
+	// fmt.Println("Style 1 :", style1)
 
 	style2 := GithubUser{
 		UserName: "Lzyct",
@@ -21,7 +35,8 @@ func main() {
 	}
 
 	fmt.Println("Style 2 :", style2)
+	style2.structFunction("ukieTux")
 
-	style3 := GithubUser{"Lzyct", "Mudassir", 69}
-	fmt.Println("Style 3 :", style3)
+	// style3 := GithubUser{"Lzyct", "Mudassir", 69}
+	// fmt.Println("Style 3 :", style3)
 }
