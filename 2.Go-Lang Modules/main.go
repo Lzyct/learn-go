1d99ac70de4a73c0834c623b7e2993a758493b2d
Lzyct <hey.mudassir@gmail.com>
2022-07-16 22:04:13 +0800

    [FEAT] Add Go Modules

---
+++ 2.Go-Lang Modules/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gomodhello "github.com/Lzyct/go-mod-hello"
+)
+
+func main() {
+	fmt.Println(gomodhello.Whatsup("Lzyct"))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Lzyct/learn-go
+
+go 1.18
+
+require github.com/Lzyct/go-mod-hello v1.0.0 // indirect
