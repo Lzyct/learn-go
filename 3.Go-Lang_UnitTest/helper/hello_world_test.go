71c28ac31a84d59b3622897d5a6f7fe243ad6302
Lzyct <hey.mudassir@gmail.com>
2022-07-26 20:51:32 +0800

    [FEAT] Sub Benchmark

--- 3.Go-Lang_UnitTest/helper/hello_world_test.go
+++ 3.Go-Lang_UnitTest/helper/hello_world_test.go
@@ -9,6 +9,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func BenchmarkSub(b *testing.B) {
+	b.Run("Lzyct", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Lzyct")
+		}
+	})
+	b.Run("Labs", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HelloWorld("Labs")
+		}
+	})
+}
+
 func BenchmarkHelloLzyct(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		HelloWorld("Lzyct")
